ec836697123251a839594734e12c216fbd6e76ba
Richard Mehlinger <richardm@dropbox.com>
2017-08-01 19:26:47 -0700

    Add stub oauth.conf

---
+++ oauth-conf.js
@@ -0,0 +1,6 @@
+exports.config = {
+  appKey: "YOUR APP KEY HERE",
+  appSecret: "YOUR APP SECRET HERE",
+  accessToken: "YOUR TOKEN HERE"
+};
+// WARNING: NEVER commit your tokens or secrets to source control.
